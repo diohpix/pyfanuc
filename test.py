f144dd0d9478bbbcfcce861ba295ccdf79e36805
c-logic <clogic@gmx.de>
2020-06-02 23:23:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ from pyfanuc import pyfanuc
 conn=pyfanuc('192.168.0.61')
 if conn.connect():
 	print("Verbunden")
+	print("Lese SPS-Wert D2204 als 16 Bit")
+	n=conn.readpmc(1,9,2204,1)
+	if n is not None:
+		print("Leitwert %0.1f" % (n[2204]/48))
+	print("Lese SPS-Wert D1870 & 1874 als 32 Bit")
+	n=conn.readpmc(2,9,1870,2)
+	if n is not None:
+		print("Laenge: %i von %i (%0.1f %%)" % (n[1870],n[1874],n[1870]/n[1874]*100))
 	print("Lese Achsen")
 	for key,val in conn.readaxis(conn.ABS | conn.SKIP | conn.REL | conn.REF).items():
 		print(key,val)
-
